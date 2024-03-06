46a2b67b967b590f16fddcb7f50c8cdeb7fa7701
ndigvijay <115492279+ndigvijay@users.noreply.github.com>
2024-03-06 18:51:13 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, world! " << std::endl;
+    std:cout  << "this is PES2UG21CS310"<<std::endl;
     return 0;
 }
